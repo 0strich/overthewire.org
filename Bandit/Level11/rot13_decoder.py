45b3cc5a5a4f68a6e12f4a792fb4f4ccc349a392
syspwn.github.io <33608588+0strich@users.noreply.github.com>
2017-11-29 01:57:42 +0900

    Update rot13_decoder.py

--- Bandit/Level11/rot13_decoder.py
+++ Bandit/Level11/rot13_decoder.py
@@ -1,1 +1,23 @@
 
+string = 'Gur cnffjbeq vf 5Gr8L4qetPEsPk8htqjhRK8XSP6x2RHh'
+decode = ''
+
+remainder = 0
+
+for i in string:
+	if(ord(i) >= ord('a') and ord(i) <= ord('z')):
+		if(ord(i) + 13 < ord('z')):
+			decode += chr(ord(i) + 13)
+		else:
+			remainder = 12 - (ord('z') - ord(i))
+			decode += chr(ord('a') + remainder)
+	elif(ord(i) >= ord('A') and ord(i) <= ord('Z')):
+
+		if(ord(i) + 13 < ord('Z')):
+			decode += chr(ord(i) + 13)
+		else:
+			remainder = 12 - (ord('Z') - ord(i))
+			decode += chr(ord('A') + remainder)
+	else: decode += i
+
+print(decode)
