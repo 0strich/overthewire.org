4815c2024d5b60f75aa15def3a2180430c77f853
syspwn.github.io <33608588+0strich@users.noreply.github.com>
2018-01-02 11:50:37 +0900

    Create Vortex0.py

---
+++ Vortex/Vortex0/Vortex0.py
@@ -0,0 +1,18 @@
+from socket import *
+from struct import *
+
+host = 'vortex.labs.overthewire.org'
+port = 5842
+add = 0
+conn=(host,port)
+
+s = socket()
+s.connect(conn)
+for i in range(4):
+	add += unpack('<I',s.recv(4))[0]
+
+s.send(pack('<I',add))
+print(s.recv(100))
+s.close()
+
+# Username: vortex1 Password: Gq#qu3bF3
