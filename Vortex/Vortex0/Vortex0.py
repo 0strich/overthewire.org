f419dd1347fc0b510b33937fb71e56304d54b32e
0strich <gee8195@naver.com>
2018-10-26 18:31:12 +0900

    update at 2018-10-26

--- Vortex/Vortex0/Vortex0.py
+++ Vortex/Vortex0/Vortex0.py
@@ -1,19 +1,17 @@
 from socket import *
 from struct import *
 
-host = 'vortex.labs.overthewire.org'
-port = 5842
 add = 0
-conn=(host,port)
 
+addr = ('vortex.labs.overthewire.org', 5842)
 s = socket()
-s.connect(conn)
+s.connect(addr)
+
 for i in range(4):
-	add += unpack('<I',s.recv(4))[0]
+    add += unpack('<I', s.recv(4))[0]
 
 s.send(pack('<I',add))
 print(s.recv(100))
 s.close()
 
 # Username: vortex1 Password: Gq#qu3bF3
-# test version
